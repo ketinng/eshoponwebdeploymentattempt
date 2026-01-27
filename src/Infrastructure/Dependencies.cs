fb6abf52751301655b46472f2d0ec040c97c458c
DESKTOP-U4CBLI4\ketin <ketinngz@gmail.com>
2026-01-27 21:14:48 +0200

    Attempt to publish after a local run

--- src/Infrastructure/Dependencies.cs
+++ src/Infrastructure/Dependencies.cs
@@ -11,7 +11,7 @@ public static class Dependencies
 {
     public static void ConfigureLocalDatabaseContexts(this IServiceCollection services, IConfiguration configuration)
     {
-        bool useOnlyInMemoryDatabase = false;
+        bool useOnlyInMemoryDatabase = true;
         if (configuration["UseOnlyInMemoryDatabase"] != null)
         {
             useOnlyInMemoryDatabase = bool.Parse(configuration["UseOnlyInMemoryDatabase"]!);
